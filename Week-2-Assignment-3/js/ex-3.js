479db89e28a18e0a0cfc2c15d342266110b01e53
Darren DeRidder <73rhodes@users.noreply.github.com>
2015-09-15 09:37:34 -0400

    Create ex-3.js

---
+++ Week-2-Assignment-3/js/ex-3.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+/****************************************
+Write a function that runs AFTER the page has 
+finished loading.  This function should write a
+message that indicates that the page is loaded.
+
+Add a second function that will run each time the 
+menu toggle image is clicked. When the function 
+runs it checks to see if the nav area is displayed
+(css style display:block). If it is not shown then 
+the function will display it, else hide it.
+
+Attach the second function to the menu toggle 
+with the click event inside your first function.
+
+Make sure you uncomment the CSS file
+where it is setting the nav menu to 
+display:none;
+****************************************/
+
+
